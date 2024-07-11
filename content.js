8369b0fdbe2c00932499bfaf86e64badf93f8284
malfestio <57299588+malfestio@users.noreply.github.com>
2024-07-11 02:45:59 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,68 +1,34 @@
-console.log("Starting content script execution.");
-
-let buttonClicked = false; // Flag to track if button has been clicked
-let observer = null; // Variable to hold the MutationObserver
-
-// Function to handle clicking the slow download button
+let buttonClicked = false; 
+let observer = null; 
 function clickSlowDownloadButton() {
-  console.log("Running clickSlowDownloadButton function");
 
   if (buttonClicked) {
-    console.log("Button already clicked once, skipping.");
-    return; // Exit function if button has already been clicked
+    return; 
   }
-
   let element = document.querySelector("#slowDownloadButton");
 
   if (element) {
-    console.log("Slow download button found, will click in 1 second.");
     setTimeout(() => {
-      console.log("Attempting to click the slow download button.");
-      console.log("Element to be clicked:", element);
-
-      // Click the button using element.click()
       element.click();
-
-      console.log("Slow download button clicked.");
-      buttonClicked = true; // Update flag to indicate button has been clicked
-
-      // Wait another 1 second before closing the tab
+      buttonClicked = true; 
       setTimeout(() => {
-        console.log("Closing the tab.");
         window.close();
-      }, 1000); // Delay of 1 second for closing the tab
-
-    }, 1000); // Delay of 1 second before clicking the button
-  } else {
-    console.log("Slow download button not found, retrying...");
-    // Optionally, retry or handle the case where the button is not found
+      }, 1000); 
+    }, 1000); 
+  } else { 
   }
 }
-
-// Function to handle when the button is found
 function handleButtonFound(mutationsList, observer) {
-  console.log("Handling button found");
 
-  // Disconnect the observer to prevent further observations
   observer.disconnect();
-
-  // Check if the #slowDownloadButton is now available and button not clicked
   let element = document.querySelector("#slowDownloadButton");
   if (element && !buttonClicked) {
-    console.log("Slow download button found by observer, will click.");
-    clickSlowDownloadButton(); // Call function to click button and close tab
+    clickSlowDownloadButton(); 
   } else {
-    console.log("Button not found or already clicked, continuing...");
   }
 }
-
-// Create a MutationObserver to watch for changes in the DOM
 observer = new MutationObserver((mutationsList, observer) => {
   handleButtonFound(mutationsList, observer);
 });
 
-// Start observing changes in the document body
 observer.observe(document.body, { childList: true, subtree: true });
-
-// Log when the script execution ends
-console.log("End of content script.");
